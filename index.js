7e3214593c8521f971c5d6553a2110876df4e282
Erick Merchant <erickmerchant@gmail.com>
2020-05-25 16:39:35 -0400

    Improve

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "arrowParens": "always",
-  "bracketSpacing": false,
-  "htmlWhitespaceSensitivity": "ignore",
-  "printWidth": 80,
-  "proseWrap": "preserve",
-  "quoteProps": "consistent",
-  "semi": false,
-  "singleQuote": true,
-  "tabWidth": 2,
-  "trailingComma": "none",
-  "useTabs": false
-}
+  arrowParens: "always",
+  bracketSpacing: false,
+  htmlWhitespaceSensitivity: "ignore",
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "consistent",
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false,
+};
